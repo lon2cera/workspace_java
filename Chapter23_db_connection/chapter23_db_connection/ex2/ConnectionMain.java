fea15a8dd9ee49c2becef59b17d9c003464f52f1
lon2cera <coco7537@naver.com>
2025-03-30 17:11:35 +0900

    Add files via upload

---
+++ Chapter23_db_connection/chapter23_db_connection/ex2/ConnectionMain.java
@@ -0,0 +1,24 @@
+package org.joonzis.ex2;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionMain {
+	public static void main(String[] args) {
+		
+		Connection conn = null;
+		
+		try {
+			conn = DBConnection.getConnection();
+			System.out.println("DB 연결 성공");
+			conn.close();
+		} catch (ClassNotFoundException e) {
+			System.out.println("jdbc 드라이버 오류");
+		} catch (SQLException e) {
+			System.out.println("DB 연결 오류");
+		} 
+		
+		
+		
+	}
+}
